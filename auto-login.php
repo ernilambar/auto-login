e0df7ba21e45d89ae5fd297a82bbb2029bb201dd
Nilambar Sharma <ernilambar@users.noreply.github.com>
2023-08-15 11:09:14 +0545

    Add files

---
+++ auto-login.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Auto Login
+Plugin URI: https://github.com/ernilambar/auto-login/
+Description: Auto login plugin
+Author: Nilambar Sharma
+Version: 1.0.0
+Author URI: https://www.nilambar.net/
+*/
