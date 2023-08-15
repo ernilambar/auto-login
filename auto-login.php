d08d57351897491785a33bb93a977a63203a0b41
Nilambar Sharma <nilambar@outlook.com>
2023-08-15 11:20:23 +0545

    Implement login

--- auto-login.php
+++ auto-login.php
@@ -2,8 +2,31 @@
 /*
 Plugin Name: Auto Login
 Plugin URI: https://github.com/ernilambar/auto-login/
-Description: Auto login plugin
+Description: Auto login MU plugin.
 Author: Nilambar Sharma
 Version: 1.0.0
 Author URI: https://www.nilambar.net/
 */
+
+/**
+ * Login by ID.
+ *
+ * @since 1.0.0
+ */
+add_action(
+  'init',
+  function() {
+    if ( isset( $_GET['autologin'] ) && absint( $_GET['autologin'] ) > 0 ) {
+      $user = get_user_by('id', absint( $_GET['autologin'] ) );
+
+      if ( false !== $user ) {
+        wp_set_current_user( $user->ID, $user->user_login );
+        wp_set_auth_cookie( $user->ID );
+        do_action( 'wp_login', $user->user_login );
+
+        wp_redirect( admin_url() );
+        exit;
+      }
+    }
+} );
+
